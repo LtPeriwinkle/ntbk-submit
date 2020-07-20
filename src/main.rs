30eaf2977ee7b974b3bfa85953638687fa614eea
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-19 20:48:38 -0700

    serve test site

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let home = warp::get().and(warp::path::end()).and(warp::fs::file("site/test.html"));
+    //serve to localhost on port 3030
+    warp::serve(home).run(([127, 0, 0, 1], 3030)).await;
 }
