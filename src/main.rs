5dc9f71dc65c98e55f18c89d4a2bc0a9c666f7ad
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-20 14:46:29 -0700

    add a second page

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use warp::Filter;
 async fn main() {
     //if route ends with not /somethings, get homepage.
     let home = warp::get().and(warp::path::end()).and(warp::fs::file("site/home.html"));
+    let test = warp::path("test").and(warp::fs::file("site/test.html"));
     //routes to css files for html to access
-    let css = warp::path("home.css").and(warp::fs::file("site/css/home.css"));
+    let ss = warp::path("home.css").and(warp::fs::file("site/css/home.css"));
     let w3 = warp::path("w3.css").and(warp::fs::file("site/css/w3.css"));
-    //all the routes in one
-    let routes = home.or(css).or(w3);
-    //serve to localhost on port 3030
+    //combining all the routes
+    let pages = home.or(test);
+    let css = ss.or(w3);
+    let routes = pages.or(css);
+    //serve to localhost:3030
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
