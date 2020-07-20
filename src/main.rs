6e004eafeb249110201256d648949b74561bd8a7
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-20 11:29:56 -0700

    comments

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    //if the address is just the main site, use test.html
+    //if route ends with not /somethings, get homepage.
     let home = warp::get().and(warp::path::end()).and(warp::fs::file("site/home.html"));
     //route to css file for html to access
     let css = warp::path("home.css").and(warp::fs::file("site/home.css"));
-
+    //all the routes in one
     let routes = home.or(css);
     //serve to localhost on port 3030
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
