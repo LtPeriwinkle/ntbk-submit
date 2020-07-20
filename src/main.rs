626c1ccb3cc720899610d0c75a93fd23e6d3a800
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-20 12:10:43 -0700

    route for w3.css

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ use warp::Filter;
 async fn main() {
     //if route ends with not /somethings, get homepage.
     let home = warp::get().and(warp::path::end()).and(warp::fs::file("site/home.html"));
-    //route to css file for html to access
-    let css = warp::path("home.css").and(warp::fs::file("site/home.css"));
+    //routes to css files for html to access
+    let css = warp::path("home.css").and(warp::fs::file("site/css/home.css"));
+    let w3 = warp::path("w3.css").and(warp::fs::file("site/css/w3.css"));
     //all the routes in one
-    let routes = home.or(css);
+    let routes = home.or(css).or(w3);
     //serve to localhost on port 3030
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
