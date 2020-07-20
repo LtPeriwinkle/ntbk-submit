028ccf0174925fe0ad903118169f14921fca5c29
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-20 10:46:41 -0700

    change test to home

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let home = warp::get().and(warp::path::end()).and(warp::fs::file("site/test.html"));
+    //if the address is just the main site, use test.html
+    let home = warp::get().and(warp::path::end()).and(warp::fs::file("site/home.html"));
     //serve to localhost on port 3030
     warp::serve(home).run(([127, 0, 0, 1], 3030)).await;
 }
