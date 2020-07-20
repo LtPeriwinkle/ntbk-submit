84ee793259a1439ca3ffecc03f480cf76f0ff7f2
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-20 11:04:39 -0700

    serve css

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use warp::Filter;
 async fn main() {
     //if the address is just the main site, use test.html
     let home = warp::get().and(warp::path::end()).and(warp::fs::file("site/home.html"));
+    //route to css file for html to access
+    let css = warp::path("home.css").and(warp::fs::file("site/home.css"));
+
+    let routes = home.or(css);
     //serve to localhost on port 3030
-    warp::serve(home).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
