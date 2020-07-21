8ffa966e5c37794f58da5da1e22ed371986ce4ca
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-21 09:30:43 -0700

    add tutorial page

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,12 @@ async fn main() {
     let home = warp::get().and(warp::path::end()).and(file("site/home.html"));
     //html for other pages
     let submit = warp::path("contribute").and(file("site/submit.html"));
+    let tutorial = warp::path("tutorial").and(file("site/howto.html"));
     //routes to css files for html to access
     let style = warp::path("home.css").and(file("site/css/home.css"));
     //w3.css is a framework that basically lets you cheat at web development
     //combining all the routes
-    let pages = home.or(submit);
+    let pages = home.or(submit).or(tutorial);
     let css = style;
     let routes = pages.or(css);
 
