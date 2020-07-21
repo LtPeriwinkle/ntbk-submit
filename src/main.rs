2d2a7061b29b4ed851b9afabd264d767c12387b7
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-20 19:06:04 -0700

    external link to w3.css

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,12 @@ async fn main() {
     //routes to css files for html to access
     let style = warp::path("home.css").and(file("site/css/home.css"));
     //w3.css is a framework that basically lets you cheat at web development
-    let w3 = warp::path("w3.css").and(file("site/css/w3.css"));
     //combining all the routes
     let pages = home.or(test);
-    let css = style.or(w3);
+    let css = style;
     let routes = pages.or(css);
 
     println!("routes initalized, server starting");
-    //serve to ip address/port specified (127, 0, 0, 1 for localhost)
-    warp::serve(routes).run(([192, 168, 0, 206], 3030)).await;
+    //serve to ip address/port specified (127, 0, 0, 1 for localhost, to see over LAN use your local ip)
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
