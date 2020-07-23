9cc7041d33b34d23fef7b9a6a6a609001cf13efb
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-23 13:23:57 -0700

    ;

--- site/js_:(/date.js
+++ site/js_:(/date.js
@@ -2,5 +2,5 @@ function setDates() {
   var d = new Date();
   document.getElementById("year").setAttribute("value", d.getFullYear().toString());
   document.getElementById("date").setAttribute("value", d.getDate().toString());
-  document.getElementById(d.getMonth().toString()).setAttribute("selected", "")
+  document.getElementById(d.getMonth()).setAttribute("selected", "");
 }
