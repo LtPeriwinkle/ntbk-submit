f8970907021618bbac3d5746b4df567158316e3a
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-07-28 21:16:50 -0700

    fix the gross js for real

--- site/js/date.js
+++ site/js/date.js
@@ -2,7 +2,6 @@ function setDates() {
   var d = new Date();
   document.getElementById("year").setAttribute("value", d.getFullYear().toString());
   document.getElementById("date").setAttribute("value", d.getDate().toString());
-  //this is a mess but i do not care
   var month = d.getMonth();
-  document.getElementById(month).setAttribute("selected", "");
+  document.getElementById(month.toString()).setAttribute("selected", "");
 }
