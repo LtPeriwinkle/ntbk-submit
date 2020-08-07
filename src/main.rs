e82070d32b72b2225960f5fbae60efec1ce8743a
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-08-07 11:49:25 -0700

    basic file serving set up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::{TcpListener, TcpStream};
+use std::fs;
 use std::io::prelude::*;
 mod components;
 use components::*;
@@ -17,8 +18,21 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = String::new();
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let req_vec: Vec<&str> = request.split(' ').collect();
+    if req_vec[0] == "GET" {
+        send_page(req_vec[1], stream);
+        println!("foo");
+    }
+}
 
-    stream.read_to_string(&mut buffer).unwrap();
-    println!("{}", buffer);
+fn send_page(path: &str, mut stream: TcpStream) {
+    let file: String;
+    match path {
+        "/" => file = fs::read_to_string("site/home.html").unwrap(),
+        _ => file = "this should not happen".to_string(),
+    }
+    stream.write(format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", file.len(), file).as_bytes()).unwrap();
 }
