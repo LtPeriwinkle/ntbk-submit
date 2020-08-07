52e243ce8a355186dc55110d842820dbf1cbc3f0
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-08-07 12:23:07 -0700

    implemented all current paths

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn handle_connection(mut stream: TcpStream) {
     let req_vec: Vec<&str> = request.split(' ').collect();
     if req_vec[0] == "GET" {
         send_page(req_vec[1], stream);
-        println!("foo");
+        println!("{}", req_vec[1]);
     }
 }
 
@@ -32,7 +32,11 @@ fn send_page(path: &str, mut stream: TcpStream) {
     let file: String;
     match path {
         "/" => file = fs::read_to_string("site/home.html").unwrap(),
-        _ => file = "this should not happen".to_string(),
+        "/home.css" => file = fs::read_to_string("site/css/home.css").unwrap(),
+        "/tutorial" => file = fs::read_to_string("site/howto.html").unwrap(),
+        "/date.js" => file = fs::read_to_string("site/js/date.js").unwrap(),
+        "/contribute" => file = fs::read_to_string("site/submit.html").unwrap(),
+        _ => file = "".to_string(),
     }
     stream.write(format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", file.len(), file).as_bytes()).unwrap();
 }
