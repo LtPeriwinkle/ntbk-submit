d6bd3199ffba10d08ab31e50b50638ba6ef8670d
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-08-19 10:39:49 -0700

    fix a lot of problems

--- site/php/submit.php
+++ site/php/submit.php
@@ -18,10 +18,6 @@
     <a href="tutorial" class="w3-bar-item w3-button w3-hover-dark-gray">tutorial</a>
   </div>
   <div id="main">
-    <h3 class="epilogue">Thank you for submitting!</h3>
-    <p>Your submission will be reviewed by the moderator of the notebook, and as long as it is not spam or wildly inappropriate, it will <br>
-      be added. Currently, you cannot preview the notebook but this feature is in the works.</p>
-  </div>
   <?php
   //put the text based stuff into a file that i'll figure out how to deal with later lol
   $folder = "/data/{$_POST["year"]}/{$_POST["month"]}/{$_POST["date"]}/{$_POST["cat"]}";
@@ -40,73 +36,16 @@
   fwrite($file, $text);
   fclose($file);
 
-  //deal with image upload for before
-  if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["bpic"]["tmp_name"]);
-    if($check !== false) {
-      $uploadOk = 1;
-    } else {
-      echo "<h3>Your 'before' image file was detected to not be an image! Please resubmit with a different file.</h3>";
-      $uploadOk = 0;
-    }
-    $bpic = "{$folder}{$_POST["title"]}-b.{$check["mime"]}";
-    if (file_exists($bpic)) {
-      echo "<h3>This submission already exists. Please try submitting again with a different title.</h3>";
-      $uploadOk = 0;
-    }
-    if ($_FILES["bpic"]["size"] > 15000000) {
-      echo "<h3>Your 'before' image file is too large. Please resubmit with a smaller image file.</h3>";
-      $uploadOk = 0;
-    }
-    if($check["mime"] != "jpg" && $check["mime"] != "png" && $check["mime"] != "jpeg") {
-      echo "<h3>Your 'before' file is not a jpg or png! Please convert the file and resubmit.</h3>";
-      $uploadOk = 0;
-    }
-    if ($uploadOk == 0) {
-      echo "<p>Because of this, your submission was not uploaded. Please try again.</p>";
-      // if everything is ok, try to upload file
-    } else {
-      if (move_uploaded_file($_FILES["bpic"]["tmp_name"], $bpic)) {
-        echo "<h3 class='epilogue'>Thank you for submitting!</h3><p>Your submission will be reviewed by the moderator of the notebook,
-        and as long as it is not spam or wildly inappropriate, it will<br>be added. Currently, you cannot preview the notebook but this
-        feature is in the works.</p>";
-      } else {
-        echo "<p>Sorry, there was an unknown error uploading your file.</p>";
-      }
-    }
-    //deal with after image
-    $check = getimagesize($_FILES["apic"]["tmp_name"]);
-    if($check !== false) {
-      $uploadOk = 1;
-    } else {
-      echo "<h3>Your 'after' image file was detected to not be an image! Please resubmit with a different file.</h3>";
-      $uploadOk = 0;
-    }
-    $apic = "{$folder}{$_POST["title"]}-a.{$check["mime"]}";
-    if (file_exists($apic)) {
-      echo "<h3>This submission already exists. Please try submitting again with a different title.</h3>";
-      $uploadOk = 0;
-    }
-    if ($_FILES["apic"]["size"] > 15000000) {
-      echo "<h3>Your 'after' image file is too large. Please resubmit with a smaller image file.</h3>";
-      $uploadOk = 0;
-    }
-    if($check["mime"] != "jpg" && $check["mime"] != "png" && $check["mime"] != "jpeg") {
-      echo "<h3>Your 'after' file is not a jpg or png! Please convert the file and resubmit.</h3>";
-      $uploadOk = 0;
-    }
-    if ($uploadOk == 0) {
-      echo "<p>Because of this, your submission was not uploaded. Please try again.</p>";
-    } else {
-      if (move_uploaded_file($_FILES["apic"]["tmp_name"], $bpic)) {
-        echo "<h3 class='epilogue'>Thank you for submitting!</h3><p>Your submission will be reviewed by the moderator of the notebook,
-        and as long as it is not spam or wildly inappropriate, it will<br>be added. Currently, you cannot preview the notebook but this
-        feature is in the works.</p>";
-      } else {
-        echo "<p>Sorry, there was an unknown error uploading your file.</p>";
-      }
-    }
+  if (is_uploaded_file($_FILES["bpic"]["tmp_name"])) {
+    move_uploaded_file($_FILES["bpic"]["tmp_name"], "{$folder}/{$_POST["title"]}-b.png");
+  }
+  if (is_uploaded_file($_FILES["apic"]["tmp_name"])) {
+    move_uploaded_file($_FILES["apic"]["tmp_name"], "{$folder}/{$_POST["title"]}-a.png");
   }
   ?>
+  <h3>Thank you for submitting!</h3>
+  <p>Your submission will be reviewed manually and added to the notebook as long as it is not spam or inappropriate. In the future, a feature will
+    be added to let you view the notebook also.</p>
+  </div>
 </body>
 </html>
