8866fcc0f11e1593d9065e09877fd13f4b640869
Fred Hancock <yxprogramming@gmail.com>
2020-08-28 05:59:41 -0700

    Finished PHP preview page

--- site/php/contents.php
+++ site/php/contents.php
@@ -9,12 +9,6 @@
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Epilogue&display=swap">
-
-  <!-- This is the markdown parsing for the preview -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/markdown-it/11.0.0/markdown-it.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/monokai.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js"></script>
-  <script src="md.js"></script>
 </head>
 
 <body>
@@ -23,17 +17,12 @@
     <a href="contribute" class="w3-bar-item w3-button w3-hover-dark-gray">contribute</a>
     <a href="tutorial" class="w3-bar-item w3-button w3-hover-dark-gray">tutorial</a>
   </div>
-  <div id="preview" onload="parse()">
-  <a href=contents.php>Table of Contents</a> 
+  <div id="main">
   <?php
-    try {
-        $file = "data/md/" . $argv[1];
-               
-        $md = file_get_contents($file);
-        echo $md;
-    } catch (exception $e) {
-        echo "# ERROR: THE FILE YOU REQUESTED DOES NOT EXIST";
-        echo "## Please click 'Table of Contents' to find valid files";
+    $files = glob("data/md/*");
+    foreach ($files as $file) {
+        $title = substr(fgets(fopen($file, 'r')), 2);
+        echo "<a href='preview.php?file=$file'>$title</a>";
     }
   ?>
   </div>
