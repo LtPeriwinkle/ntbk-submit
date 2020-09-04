70e5a96b6e65f74412de4ebc8af24281417aa4bb
Fred Hancock <yxprogramming@gmail.com>
2020-09-03 19:15:12 -0700

    PHP preview started

--- site/js/md.js
+++ site/js/md.js
@@ -1,17 +1,23 @@
 function parse() {
-	  var md = window.markdownit("commonmark", {
-	    highlight: function (str, lang) {
-		  if (lang && hljs.getLanguage(lang)) {
-		    try {
-		      return "<pre><code>" + hljs.highlight(lang, str, true).value + "</code></pre>";
-			} catch (_) {}
-		  }
+    var md = window.markdownit('commonmark', {
+        highlight: function (str, lang) {
+	    if (lang && hljs.getLanguage(lang)) {
+	        try {
+	            return '<pre><code>' + hljs.highlight(lang, str, true).value + '</code></pre>';
+		    } catch (_) {}
+	    }
 
-		  return "<pre><code>" + md.utils.escapeHtml(str) + "</code></pre>";
-	  	},
-		breaks: true
-	  });
-	  console.log(md.render(document.getElementById('desc').value));
-    var title = '## '.concat(document.getElementById('title').value);
-	  document.getElementById('preview').innerHTML = md.render(title).concat(md.render(document.getElementById('desc').value));
-	}
+	    return "<pre><code>" + md.utils.escapeHtml(str) + '</code></pre>';
+  	    },
+	    breaks: true
+    });
+    if (document.getElementById('desc')) {
+        var title = '# ' + document.getElementById('title').value;
+        var date = document.getElementById('month').value + "/" + document.getElementById('date').value + "/" + document.getElementById('year').value;
+        var subHead = document.getElementById('category').value + " by " + document.getElementById('contrib').value + " on " + date;
+	    document.getElementById('preview').innerHTML = md.render(title).concat(md.render(subHead)).concat(md.render(document.getElementById('desc').value));
+	} else {
+        var inner = document.getElementById('preview').value;
+        document.getElementById('preview').innerHtml = md.render(inner);
+    }
+}
---
+++ site/php/preview.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>9573X Notebook</title>
+  <meta charset="UTF-8">
+  <meta name="author" content="periwinkle">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="home.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Epilogue&display=swap">
+</head>
+
+<body>
+  <div id="nav" class="w3-bar w3-black w3-text-white w3-bottom-bar w3-border-black">
+    <a href=".." class="w3-bar-item w3-button w3-hover-dark-gray">home</a>
+    <a href="contribute" class="w3-bar-item w3-button w3-hover-dark-gray">contribute</a>
+    <a href="tutorial" class="w3-bar-item w3-button w3-hover-dark-gray">tutorial</a>
+  </div>
+  <div id="preview">
+  <?php
+    $file = $argv[1];
+    $md = file_get_contents($file);
+    echo $md;
+  ?>
+  </div>
+</body>
+</html>
