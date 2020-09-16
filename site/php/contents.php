4832b39d5a623c822afe1602c65fbdbfa4fd1647
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-09-16 13:34:56 -0700

    change link style

--- site/php/contents.php
+++ site/php/contents.php
@@ -9,6 +9,14 @@
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Epilogue&display=swap">
+  <style>
+    a:link {
+      text-decoration: none;
+    }
+    a:hover {
+      text-decoration: underline;
+    }
+  </style>
 </head>
 
 <body>
