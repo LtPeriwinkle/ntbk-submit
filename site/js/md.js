212e30eec71b3045b8cb8c2594d6705b2435737a
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-09-16 13:43:30 -0700

    fix date previewing

--- site/js/md.js
+++ site/js/md.js
@@ -13,7 +13,7 @@ function parse() {
     });
     if (window.location.href == "https://apexnotebook.ml/contribute") {
         var title = '# ' + document.getElementById('title').value;
-        var date = document.getElementById('months').value + "/" + document.getElementById('date').value + "/" + document.getElementById('year').value;
+        var date = document.getElementById('date').value;
         var subHead = document.getElementById('cat').value + " by " + document.getElementById('name').value + " on " + date;
 	    document.getElementById('preview').innerHTML = md.render(title).concat(md.render(subHead)).concat(md.render(document.getElementById('desc').value));
 	} else {
